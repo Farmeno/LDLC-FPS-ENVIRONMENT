23fb1c52d916f8d5eb99578304e4cc6d14f41692
DESKTOP-VTKHO89\LDLC <timothee.pennec4811@gmail.com>
2023-04-07 11:34:05 +0200

    V1

--- Assets/recolte.cs
+++ Assets/recolte.cs
@@ -11,7 +11,7 @@ public class recolte : MonoBehaviour
     public Camera camera;
     public int score;
     public string affiche_score;
-    public float maxDistance = 0.2f;
+    public float maxDistance = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,23 +29,28 @@ public class recolte : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                if(hit.collider.CompareTag("Cube") && Vector3.Distance(Camera.main.transform.position, hit.collider.transform.position)<=maxDistance)
+                if (hit.collider.CompareTag("Cube"))
                 {
+                    var d = Vector3.Distance(Camera.main.transform.position, hit.collider.transform.position);
+                    Debug.Log(d);
+                    if (d <= maxDistance)
+                    {
 
-                Debug.Log("Bravo !");
-                score += 1;
-                affiche_score = score + "/?" ;
-                GetComponent<TMP_Text>().text = affiche_score;
+                        Debug.Log("Bravo !");
+                        score += 1;
+                        affiche_score = score + "/?";
+                        GetComponent<TMP_Text>().text = affiche_score;
 
-                    Destroy(hit.collider.gameObject, 1f);
+                        Destroy(hit.collider.gameObject, 1f);
+
+                    }
+                    if (score > 3)
+                    {
+                        affiche_score = score + "/" + score;
+                        GetComponent<TMP_Text>().text = affiche_score;
+                    }
 
                 }
-                if (score > 3)
-                 {
-                    affiche_score = score + "/" + score;
-                    GetComponent<TMP_Text>().text = affiche_score;
-                }
-
             }
         }
 
